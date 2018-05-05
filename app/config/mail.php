d4add5b51c9829d2f80d710ffe869e3fcf9ebbac
sebasmck <corporate.att@gmail.com>
2018-05-05 16:40:37 -0500

    smtp added

--- app/config/mail.php
+++ app/config/mail.php
@@ -29,7 +29,7 @@ return [
     |
     */
 
-    'host' => env('MAIL_HOST', 'mail.anaxanet.com'),
+    'host' => env('MAIL_HOST', 'smtp.gmail.com'),
 
     /*
     |--------------------------------------------------------------------------
